e17b7e5dd89d719575c0f266a577893edf804805
Alien <alongejoshua@gmail.com>
2023-06-02 22:48:10 +0100

    follows the space and code of eslint

--- utils/db.js
+++ utils/db.js
@@ -5,30 +5,28 @@ const PORT = process.env.DB_PORT || 27017;
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${HOST}:${PORT}`;
 
-
 class DBClient {
-    constructor() {
-        this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
-        this.client.connect().then(() => {
-            this.db = this.client.db(`${DATABASE}`);
-        }).catch((err) => console.log(err));
-    }
+  constructor() {
+    this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(`${DATABASE}`);
+    }).catch((err) => console.log(err));
+  }
 
-    isAlive() {
-        return this.client.isConnected();
-    }
+  isAlive() {
+    return this.client.isConnected();
+  }
 
+  async nbUsers() {
+    const userNum = await this.db.collection('users').countDocuments();
+    return userNum;
+  }
 
-    async nbUsers() {
-        const userNum = await this.db.collection('users').countDocuments();
-        return userNum;
-    }
-
-    async nbFiles() {
-        const filesNum =  await this.db.collection('files').countDocuments();
-        return filesNum;
-    }
-};
+  async nbFiles() {
+    const filesNum = await this.db.collection('files').countDocuments();
+    return filesNum;
+  }
+}
 
 const dbClient = new DBClient();
 
